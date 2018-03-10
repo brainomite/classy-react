0b9287142060a7b305175efba9d1ec2c180cba93
Aaron Young <brainomite@gmail.com>
2018-03-10 17:27:59 -0500

    changing to selection

--- commands/convertToClass.js
+++ commands/convertToClass.js
@@ -3,25 +3,37 @@ const vscode = require('vscode');
 const Position = vscode.Position
 const Range = vscode.Range
 const babel = require('babel-core')
+
 const { test1 } = require('../babelPlugins')
 
+const errorMessage = msg => {
+  vscode.window.showErrorMessage(msg)
+}
 
 module.exports = () => {
   const editor = vscode.window.activeTextEditor
-  const eDocument = editor.document
-  const code = eDocument.getText()
-  const lastLine = eDocument.lineCount - 1
-  const lastPos = eDocument.lineAt(lastLine).range.end
-  const entireDoc = new Range(new Position(0, 0), new Position(lastLine, lastPos))
-  const newCode = code.split('\n').map((line, index) => (index + 1) + line ).join('\n')
+  const selection = editor.selection
+  if (selection.isEmpty){
+    errorMessage('Please select some code and try again')
+  } else {
+    const eDocument = editor.document
+    const code = eDocument.getText(selection)
+    const lastLine = eDocument.lineCount - 1
+    const lastPos = eDocument.lineAt(lastLine).range.end
+    const entireDoc = new Range(new Position(0, 0), new Position(lastLine, lastPos))
+    console.log('editor.selection: ', editor.selection);
 
-  const result = babel.transform(code, {
-    plugins: [
-      test1,
-    ]
-  })
+    const result = babel.transform(code, {
+      plugins: [
+        test1,
+      ]
+    }, {
+      plugins: ['jsx']
+    })
 
-  editor.edit(editbuilder => {
-    editbuilder.replace(entireDoc, result.code)
-  })
+    editor.edit(editbuilder => {
+      editbuilder.replace(selection, result.code)
+    })
+
+  }
 }
