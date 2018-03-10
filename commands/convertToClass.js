8bf75c172f2359c814d646cb4ebc717ee38779d9
Aaron Young <brainomite@gmail.com>
2018-03-10 15:03:26 -0500

    goal achieved

--- commands/convertToClass.js
+++ commands/convertToClass.js
@@ -1,7 +1,18 @@
 'use strict'
 const vscode = require('vscode');
+const Position = vscode.Position
+const Range = vscode.Range
+
 
 module.exports = () => {
-  // todo
-  vscode.window.showInformationMessage('I work! pretending to convert to class');
+  const editor = vscode.window.activeTextEditor
+  const eDocument = editor.document
+  const code = eDocument.getText()
+  const lastLine = eDocument.lineCount - 1
+  const lastPos = eDocument.lineAt(lastLine).range.end
+  const entireDoc = new Range(new Position(0, 0), new Position(lastLine, lastPos))
+  const newCode = code.split('\n').map((line, index) => (index + 1) + line ).join('\n')
+  editor.edit(editbuilder => {
+    editbuilder.replace(entireDoc, newCode)
+  })
 }
