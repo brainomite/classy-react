3b353bf51328c1444d05aa764addcc134a79d0f4
Aaron Young <brainomite@gmail.com>
2018-03-11 10:53:48 -0400

    react found

--- commands/convertToClass.js
+++ commands/convertToClass.js
@@ -1,6 +1,8 @@
 'use strict'
 const vscode = require('vscode');
 const babel = require('babel-core')
+const traverse = require('babel-traverse').default;
+const babylon = require('babylon');
 
 const { jsxFunctionToClass, test1, } = require('../babelPlugins')
 
@@ -17,16 +19,24 @@ module.exports = () => {
     const eDocument = editor.document
     const code = eDocument.getText(selection)
 
-    const result = babel.transform(code, {
-      plugins: [
-        jsxFunctionToClass,
-      ],
-      parserOpts: {
-        plugins: ['jsx', ],
-      },
-    })
-    console.log('result: ', result.code);
+    // const result = babel.transform(code, {
+    //   plugins: [
+    //     jsxFunctionToClass,
+    //   ],
+    //   parserOpts: {
+    //     plugins: ['jsx', ],
+    //   },
+    // })
+    // console.log('result: ', result.code);
+    let ast = babylon.parse(code,{
+      plugins: ['jsx', ],
+    });
 
+    traverse(ast, {
+        JSXElement(path) {
+            console.log('entered');
+        }
+    });
     // editor.edit(editbuilder => {
     //   editbuilder.replace(selection, result.code)
     // })
