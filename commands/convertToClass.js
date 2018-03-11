f0277edefd51a5c87ee3522b1080642fc1415190
Aaron Young <brainomite@gmail.com>
2018-03-11 12:16:07 -0400

    not only found but it works!

--- commands/convertToClass.js
+++ commands/convertToClass.js
@@ -1,15 +1,25 @@
 'use strict'
-/* eslint-disable no-unused-vars, no-useless-return */
+/* eslint-disable no-unused-vars */
 const vscode = require('vscode');
 const babel = require('babel-core')
 const traverse = require('babel-traverse').default;
 const babylon = require('babylon');
+const template = require('babel-template');
+const generate = require('babel-generator').default
 
-const { jsxFunctionToClass, test1, } = require('../babelPlugins')
+const types = babel.types
+
+const { jsxFunctionToClass, } = require('../babelPlugins')
 
 const errorMessage = msg => {
   vscode.window.showErrorMessage(msg)
 }
+
+const generateResultObj = () => ({
+  jsxFound: false,
+  functionParentLocated: false,
+  declarationLocated: false,
+})
 
 module.exports = () => {
   const editor = vscode.window.activeTextEditor
@@ -19,36 +29,35 @@ module.exports = () => {
   } else {
     const eDocument = editor.document
     const code = eDocument.getText(selection)
-    const resultObj = {
-      jsxFound: false,
-      functionParentLocated: false,
-    }
-    // const result = babel.transform(code, {
-    //   plugins: [
-    //     jsxFunctionToClass,
-    //   ],
-    //   parserOpts: {
-    //     plugins: ['jsx', ],
-    //   },
-    // })
-    // console.log('result: ', result.code);
-    let ast = babylon.parse(code, {
+    const resultObj = generateResultObj()
+
+    const ast = babylon.parse(code, {
       plugins: ['jsx', ],
     });
     const relevantNodes = {}
     traverse(ast, {
         JSXElement(jsxPath) {
           resultObj.jsxFound = true
-          const parentPath = jsxPath.findParent((pPath) => {
+          const funcPath = jsxPath.findParent((pPath) => {
             return pPath.isFunctionExpression()
           })
-          if (parentPath) {
+          if (funcPath) {
             resultObj.functionParentLocated = true
           } else {
             return
           }
-          relevantNodes.body = parentPath.node.body
-          relevantNodes.identifierName = parentPath.parent.id.name
+          const declarationPath = funcPath.findParent(aPath => {
+            return aPath.isVariableDeclaration()
+          })
+          if (declarationPath){
+            resultObj.declarationLocated = true
+          } else {
+            return
+          }
+
+          relevantNodes.identifierName = funcPath.parent.id.name
+          relevantNodes.body = funcPath.node.body
+          relevantNodes.pathToReplace = declarationPath
         },
     });
 
@@ -58,10 +67,30 @@ module.exports = () => {
     } else if (!resultObj.functionParentLocated){
       errorMessage('Classy React was unable to locate the JSX function to convert')
       return
+    } else if (!resultObj.declarationLocated){
+      errorMessage('Classy React was unable to loacte the JSX function declarations. Aborting')
     }
-    // editor.edit(editbuilder => {
-    //   editbuilder.replace(selection, result.code)
-    // })
+
+    const buildRequire = template(`
+      class CLASS_NAME{
+        constructor(props){
+          super(props)
+          this.state = undefined
+        }
+        RENDER_METHOD
+      }
+    `);
+
+    const newAst = buildRequire({
+      CLASS_NAME: types.identifier(relevantNodes.identifierName),
+      RENDER_METHOD: types.classMethod('method', types.identifier('render'), [], relevantNodes.body),
+    });
+    console.log('newAst: ', newAst);
+    relevantNodes.pathToReplace.replaceWith(newAst)
+    const result = generate(ast)
+    editor.edit(editbuilder => {
+      editbuilder.replace(selection, result.code)
+    })
 
   }
 }
